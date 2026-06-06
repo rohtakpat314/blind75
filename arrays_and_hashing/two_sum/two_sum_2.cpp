22072d05249d039593a864e194b155ef8e3c6738
Rohtak Patwardhan <rohtak.pat314@gmail.com>
2026-06-06 17:18:52 -0400

    Implement twoSum function in two_sum_2.cpp

---
+++ arrays_and_hashing/two_sum/two_sum_2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            int temp = nums[i]; 
+            
+            for (int j = i+1; j < nums.size(); j++) { 
+                if (target == temp + nums[j]) { 
+                    return {i, j};
+                }
+            }
+        }
+        return {}; 
+    }     
+};
