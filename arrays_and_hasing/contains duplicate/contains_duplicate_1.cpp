a33684f90285f6df09e2d67e05d0ae6b6d9e5fa9
Rohtak Patwardhan <rohtak.pat314@gmail.com>
2026-06-06 15:13:50 -0400

    Create contains_duplicate_1.cpp

---
+++ arrays_and_hasing/contains duplicate/contains_duplicate_1.cpp
@@ -0,0 +1,16 @@
+// O(n^2) and O(1) solved using array indexing 
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+    
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (nums[i] == nums[j]) return true;
+            }
+        }
+
+    return false; 
+    }
+    
+};
